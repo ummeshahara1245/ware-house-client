4aac65f3e4899fd9ab8dac10cec50a189b0eb9f6
umme shahara <ummeshahara1245@gmail.com>
2022-05-07 15:26:09 +0600

    banner-done

--- src/pages/Banner/Banner.js
+++ src/pages/Banner/Banner.js
@@ -1,72 +1,45 @@
 import React from 'react';
-import image1 from "../../images/bali.png"
-import image2 from "../../images/kuakata.png"
-import image3 from "../../images/meeru.png"
+import Grid from '@mui/material/Grid';
+import imge from '../../images/New Project (4).png';
+import { Container, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
 import './Banner.css'
+import { Link } from 'react-router-dom';
+const verticalCenter = {
+    display: 'flex',
+    alignItems: 'center',
+    height: 400
+}
+
+
 const Banner = () => {
     return (
-        <div className="">
-        <section class="carousel" aria-label="Gallery">
-  <ol class="carousel__viewport">
-    <li id="carousel__slide1"
-        tabindex="0"
-        class="carousel__slide">
-      <div class="carousel__snapper">
-        <a href="#carousel__slide4"
-           class="carousel__prev">Go to last slide</a>
-        <a href="#carousel__slide2"
-           class="carousel__next">Go to next slide</a>
-      </div>
-    </li>
-    <li id="carousel__slide2"
-        tabindex="0"
-        class="carousel__slide">
-      <div class="carousel__snapper"></div>
-      <a href="#carousel__slide1"
-         class="carousel__prev">Go to previous slide</a>
-      <a href="#carousel__slide3"
-         class="carousel__next">Go to next slide</a>
-    </li>
-    <li id="carousel__slide3"
-        tabindex="0"
-        class="carousel__slide">
-      <div class="carousel__snapper"></div>
-      <a href="#carousel__slide2"
-         class="carousel__prev">Go to previous slide</a>
-      <a href="#carousel__slide4"
-         class="carousel__next">Go to next slide</a>
-    </li>
-    <li id="carousel__slide4"
-        tabindex="0"
-        class="carousel__slide">
-      <div class="carousel__snapper"></div>
-      <a href="#carousel__slide3"
-         class="carousel__prev">Go to previous slide</a>
-      <a href="#carousel__slide1"
-         class="carousel__next">Go to first slide</a>
-    </li>
-  </ol>
-  <aside class="carousel__navigation">
-    <ol class="carousel__navigation-list">
-      <li class="carousel__navigation-item">
-        <a href="#carousel__slide1"
-           class="carousel__navigation-button">Go to slide 1</a>
-      </li>
-      <li class="carousel__navigation-item">
-        <a href="#carousel__slide2"
-           class="carousel__navigation-button">Go to slide 2</a>
-      </li>
-      <li class="carousel__navigation-item">
-        <a href="#carousel__slide3"
-           class="carousel__navigation-button">Go to slide 3</a>
-      </li>
-      <li class="carousel__navigation-item">
-        <a href="#carousel__slide4"
-           class="carousel__navigation-button">Go to slide 4</a>
-      </li>
-    </ol>
-  </aside>
-</section>
+        <div style={{height:"500px"}} className='bgimage'>
+            <div style={{paddingTop:'70px'}}>
+            <Container className='shadow-lg' sx={{ flexGrow: 1,height:'400px',borderRadius:'20px',}}>
+            <Grid className='mediqe' container spacing={2}>
+                <Grid item style={{ ...verticalCenter, textAlign: 'left',height:'524px' }} xs={12} md={6}>
+                    <Box style={{marginTop:"-100px"}}>
+                        <Typography variant='h4' style={{fontFamily:'Pacifico, cursive'}}>
+                          ORGANIC
+                            <br />
+                           VEGETABLES
+                            <br />
+                        </Typography>
+                        <h5 className='text-black'>
+                           Small Changes Big Difference
+                        </h5>
+                        <Link className='no-underline' to='/explore'><button className='button-off rounded-full'>Explore our products<i className="fa-solid fa-magnifying-glass ico"></i></button></Link>
+                    </Box>
+                </Grid>
+                <Grid className='imgGrid' item style={{ ...verticalCenter, textAlign: 'left' }} xs={12} md={6}>
+                    <img className='ibu' src={imge} alt="" />
+                </Grid>
+            </Grid>
+                
+
+        </Container>
+            </div>
         </div>
     );
 };
